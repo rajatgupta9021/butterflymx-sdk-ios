bd87dcb0a9cf78c0f4704755edf9bbff24a4651e
Rajat Gupta <rajat.gupta@vts.com>
2026-05-25 04:22:32 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,8 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-var package = Package(
+let package = Package(
     name: "ButterflyMX",
     platforms: [
         .iOS(.v13)
@@ -11,14 +10,17 @@ var package = Package(
     products: [
         .library(
             name: "BMXCore",
-            targets: [ "BMXCore" ]),
+            targets: ["BMXCore"]
+        ),
         .library(
             name: "BMXCall",
-            targets: [ "BMXCall" ]),
+            targets: ["BMXCall"]
+        ),
     ],
     dependencies: [
-         .package(url: "https://github.com/OAuthSwift/OAuthSwift.git", .upToNextMajor(from: "2.2.0")), 
-         .package(url: "https://github.com/infinum/Japx.git", .upToNextMajor(from: "4.0.0"))
+        .package(url: "https://github.com/OAuthSwift/OAuthSwift.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/infinum/Japx.git", .upToNextMajor(from: "4.0.0")),
+        .package(name: "TwilioVideo", url: "https://github.com/twilio/twilio-video-ios.git", .upToNextMajor(from: "5.8.1"))
     ],
     targets: [
         .target(
@@ -26,16 +28,23 @@ var package = Package(
             dependencies: [
                 .product(name: "OAuthSwift", package: "OAuthSwift"),
                 .product(name: "Japx", package: "Japx"),
-                .product(name: "JapxAlamofire", package: "Japx"),
+                .product(name: "JapxAlamofire", package: "Japx")
             ],
             path: "BMXCore",
-            resources: [.process("PrivacyInfo.xcprivacy")]
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ]
         ),
         .target(
             name: "BMXCall",
-            dependencies: ["BMXCore"],
+            dependencies: [
+                "BMXCore",
+                "TwilioVideo"
+            ],
             path: "BMXCall",
-            resources: [.process("PrivacyInfo.xcprivacy")]
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ]
         )
     ]
 )
